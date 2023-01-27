7d72b8b5cb3cc42d63aa2d075cbdbfaee3065a7f
Paulo-21 <paul.cibier@gmail.com>
2023-01-27 20:00:08 +0100

    Hello

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    
+    let mut King : u64 = 0b000000_000000_000000_000000_000000_000100;
+    println!("{:b}", board);
+    println!("{}", board);
+
+    board<<=1;
+    println!("{:b}", board);
+    println!("{}", board);
+}
