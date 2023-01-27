637d843b3d7eb27828e341329c169485546e353c
Paulo-21 <paul.cibier@gmail.com>
2023-01-27 23:58:47 +0100

    init board

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,97 @@
+#[allow(clippy::too_many_arguments)]
+fn array_to_bitboard(chessboard : [[char;8]; 8], wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) {
+    let mut i = 0;
+    for v in chessboard {
+        for c in v {
+            match c {
+                'p' => { *wp += convert_string_to_bitboard(i); },
+                'n' => { *wn += convert_string_to_bitboard(i); },
+                'b' => { *wb += convert_string_to_bitboard(i); },
+                'r' => { *wr += convert_string_to_bitboard(i); },
+                'q' => { *wq += convert_string_to_bitboard(i); },
+                'k' => { *wk += convert_string_to_bitboard(i); },
+                'P' => { *bp += convert_string_to_bitboard(i); },
+                'N' => { *bn += convert_string_to_bitboard(i); },
+                'B' => { *bb += convert_string_to_bitboard(i); },
+                'R' => { *br += convert_string_to_bitboard(i); },
+                'Q' => { *bq += convert_string_to_bitboard(i); },
+                'K' => { *bk += convert_string_to_bitboard(i); },
+                _ => {}
+            }
+            i+=1;
+        }
+    }
+}
+#[allow(clippy::too_many_arguments)]
+fn draw_board(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) {
+    let mut chess_board:[[char;8];8] = [[' ';8];8];
+    let mut i = 0;
+    for x in &mut chess_board {
+        for c in x {
+            if ((*wp >> i) & 1) == 1  { *c = 'P'; }
+            if ((*wn >> i) & 1) == 1  { *c = 'N'; }
+            if ((*wb >> i) & 1) == 1  { *c = 'B'; }
+            if ((*wr >> i) & 1) == 1  { *c = 'R'; }
+            if ((*wq >> i) & 1) == 1  { *c = 'Q'; }
+            if ((*wk >> i) & 1) == 1  { *c = 'K'; }
+            if ((*bp >> i) & 1) == 1  { *c = 'p'; }
+            if ((*bn >> i) & 1) == 1  { *c = 'n'; }
+            if ((*bb >> i) & 1) == 1  { *c = 'b'; }
+            if ((*br >> i) & 1) == 1  { *c = 'r'; }
+            if ((*bq >> i) & 1) == 1  { *c = 'q'; }
+            if ((*bk >> i) & 1) == 1  { *c = 'k'; }
+            i+=1;
+        }
+    }
+    let letter = 'a';
+    print!("    ");
+    for i in 0..8 {
+        print!("{} ", (letter as u8+i) as char);
+    }
+    print!("\n   ");
+    for _i in 0..8 {
+        print!("__");
+    }
+    println!();
+    for (i, x) in chess_board.iter().enumerate() {
+        print!("{i} | ");
+        for c in x {
+            print!("{c} ");
+        }
+        println!();
+    }
+}
+fn convert_string_to_bitboard(binary:usize) -> u64 {
+    u64::pow(2, (63-binary) as u32)
+}
 fn main() {
     println!("Hello, world!");
+    let chess_board:[[char;8];8] = [
+        ['r','n','b','q','k','b','n','r'],
+        ['p','p','p','p','p','p','p','p'],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        ['P','P','P','P','P','P','P','P'],
+        ['R','N','B','Q','K','B','N','R'],
+    ];
     
-    let mut King : u64 = 0b000000_000000_000000_000000_000000_000100;
-    println!("{:b}", board);
-    println!("{}", board);
+    let mut wp : u64 = 0;
+    let mut wn : u64 = 0;
+    let mut wb : u64 = 0;
+    let mut wr : u64 = 0;
+    let mut wq : u64 = 0;
+    let mut wk : u64 = 0;
+    let mut bp : u64 = 0;
+    let mut bn : u64 = 0;
+    let mut bb : u64 = 0;
+    let mut br : u64 = 0;
+    let mut bq : u64 = 0;
+    let mut bk : u64 = 0;
 
-    board<<=1;
-    println!("{:b}", board);
-    println!("{}", board);
+    array_to_bitboard(chess_board, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+
+    //println!("{:b}", wr);
+    draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
 }
