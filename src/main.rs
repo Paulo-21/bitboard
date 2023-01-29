d60ec106ae61e5d36d562e9d7cc6a2a893718779
Paulo-21 <paul.cibier@gmail.com>
2023-01-29 22:47:10 +0100

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::io;
+
 static FILE_A:u64 = 72340172838076673;
 static RANK_8:u64 = 255;
+//static RANK_1:u64 = 280375465082880;
 
 
 #[allow(clippy::too_many_arguments)]
@@ -58,7 +61,7 @@ fn draw_board(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, w
     }
     println!();
     for (i, x) in chess_board.iter().enumerate() {
-        print!("{i} | ");
+        print!("{} | ", i+1);
         for c in x {
             print!("{c} ");
         }
@@ -66,12 +69,31 @@ fn draw_board(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, w
     }
 }
 fn convert_string_to_bitboard(binary:usize) -> u64 {
-    u64::pow(2, (63-binary) as u32)
+    u64::pow(2, (binary) as u32)
 }
-fn possibilityWP(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) {
+fn possibility_wp(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> u64 {
+    let black = *bp | *bn | *bb | *br | *bq | *bk;
+    let white = *wp | *wn | *wb | *wr | *wq | *wk;
+    let pmoves1 = *wp<<7 & black & !RANK_8;
+    let pmoves2 = *wp<<9 & black & !RANK_8;
+    let pmoves3 = *wp<<8 & !(black | white);
+    pmoves1 | pmoves2 | pmoves3
+}
+fn possibility_bp(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> u64 {
+    let black = *bp | *bn | *bb | *br | *bq | *bk;
+    let white = *wp | *wn | *wb | *wr | *wq | *wk;
+    let pmoves1 = *bp>>7 & white ;//& !RANK_1;
+    let pmoves2 = *bp>>9 & white ;//& !RANK_1;
+    let pmoves3 = *bp>>8 & !(white | black);
+    pmoves1 | pmoves2 | pmoves3
+}
+fn possibility_wn(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> u64 {
     let black = *bp & *bn & *bb & *br & *bq & *bk;
-
-    let pmoves = *wp>>7 & black & !RANK_8;
+    let white = *wp & *wn & *wb & *wr & *wq & *wk;
+    let pmoves1 = *wp>>7 & black & !RANK_8;
+    let pmoves2 = *wp>>9 & black & !RANK_8;
+    let pmoves3 = *wp>>8 & !black & !white;
+    pmoves1 & pmoves2 & pmoves3
 }
 fn convert_move_to_bitboard(moves : &str) -> (u64,u64) {
     let piece = &moves[0..2];
@@ -82,15 +104,131 @@ fn convert_move_to_bitboard(moves : &str) -> (u64,u64) {
     let quatre = *&move_to[0..2].chars().nth(1).unwrap() as u64-48;
     let a = u64::pow(2, ((deux-1) *8 +  un-1 )as u32);
     let b = u64::pow(2, ((quatre-1) *8 +  trois-1)as u32);
-
+    println!("a : {:b} \nb : {:b}", a, b);
     (a,b)
+}
+
+fn compute_move_w(a:u64, b:u64, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> bool {
+    let black = *bp & *bn & *bb & *br & *bq & *bk;
+    if ((*wp) & a) != 0 {
+        let mut p = (*wp) & a;
+        
+        let moves = possibility_wp(&mut p, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
+        println!("M : {:b}", moves);
+
+        if moves & b != 0 {
+            (*wp) = (*wp) & (!a);
+            (*wp) = (*wp) | b;
+            if black & b != 0 {
+                if *bp & b != 0 {
+                    *bp &= !b;
+                }
+                else if *bn & b != 0 {
+                    *bn &= !b;
+                }
+                else if *bb & b != 0 {
+                    *bb &= !b;
+                }
+                else if *br & b != 0 {
+                    *br &= !b;
+                }
+                else if *bq & b != 0 {
+                    *bq &= !b;
+                }
+            }
+            true
+        }
+        else {
+            false
+        }
+    }
+    else if *wn & a != 0 {
+        possibility_wn(wp, &mut (*wn & a), wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
+        false
+    }
+    else if *wb & a != 0 {
+        false
+    }
+    else if *wr & a != 0 {
+       false 
+    }
+    else if *wq & a != 0 {
+        false
+    }
+    else if *wk & a != 0 {
+        false
+    }
+    else if *wn & a != 0 {
+        false
+    }
+    else {
+        false
+    }
+}
+fn compute_move_b(a:u64, b:u64, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> bool {
+    let black = *bp & *bn & *bb & *br & *bq & *bk;
+    let white = *wp & *wn & *wb & *wr & *wq & *wk;
+    if ((*bp) & a) != 0 {
+        let mut p = (*bp) & a;
+        
+        let moves = possibility_bp(wp, wn, wb, wr, wq, wk, &mut p, bn, bb, br, bq, bk);
+        println!("M : {:b}", moves);
+
+        if moves & b != 0 {
+            (*bp) &= !a;
+            (*bp) |= b;
+            if white & b != 0 {
+                if *wp & b != 0 {
+                    *wp &= !b;
+                }
+                else if *wn & b != 0 {
+                    *wn &= !b;
+                }
+                else if *wb & b != 0 {
+                    *wb &= !b;
+                }
+                else if *wr & b != 0 {
+                    *wr &= !b;
+                }
+                else if *wq & b != 0 {
+                    *wq &= !b;
+                }
+            }
+            true
+        }
+        else {
+            false
+        }
+    }
+    else if *wn & a != 0 {
+        possibility_wn(wp, &mut (*wn & a), wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
+        false
+    }
+    else if *wb & a != 0 {
+        false
+    }
+    else if *wr & a != 0 {
+       false 
+    }
+    else if *wq & a != 0 {
+        false
+    }
+    else if *wk & a != 0 {
+        false
+    }
+    else if *wn & a != 0 {
+        false
+    }
+    else {
+        false
+    }
 }
 fn main() {
     println!("Hello, world!");
     let chess_board:[[char;8];8] = [
         ['r','n','b','q','k','b','n','r'],
         ['p','p','p','p','p','p','p','p'],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ','P',' ',' ',' ',' '],
         [' ',' ',' ',' ',' ',' ',' ',' '],
         [' ',' ',' ',' ',' ',' ',' ',' '],
         [' ',' ',' ',' ',' ',' ',' ',' '],
@@ -110,13 +248,32 @@ fn main() {
     let mut br : u64 = 0;
     let mut bq : u64 = 0;
     let mut bk : u64 = 0;
-
     array_to_bitboard(chess_board, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
 
-    //println!("{:b}", wr);
-    //draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
-
-    let play_move = "a2h8";
-    let (a,b) = convert_move_to_bitboard(play_move);
-    println!("{:b} \n{:b}",a ,b);
+    println!("{:b}", wp);
+    let mut white_to_play = true;
+    //let play_move = "e2e3";
+    //let (a,b) = convert_move_to_bitboard(play_move);
+    //println!("WP : {:b}", a&wp);
+    let moves = ["e2e3", "d7d6", "e3e4", "d6d5", "e4d5", "d5d4"];
+    draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+    for m in moves {
+        //let mut buffer = String::new();
+        //io::stdin().read_line(&mut buffer).unwrap();
+        let (a,b) = convert_move_to_bitboard(&m);
+        let response;
+        if white_to_play {
+            response = compute_move_w(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+        }
+        else {
+            response = compute_move_b(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+        }
+        white_to_play ^= response;
+        
+        //println!("{:b} \n{:b}",a ,b);
+        println!("{}", m);
+        println!("{}", response);
+        draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+    }
+    
 }
