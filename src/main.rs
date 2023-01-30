f833d7e8d2338840cc935e5d899c280d418661c3
Paulo-21 <paul.cibier@gmail.com>
2023-01-30 20:40:23 +0100

    couille

--- src/main.rs
+++ src/main.rs
@@ -96,28 +96,29 @@ fn possibility_bp(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u6
 fn possibility_wn(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> u64 {
     //let black = *bp | *bn | *bb | *br | *bq | *bk;
     let white = *wp | *wn | *wb | *wr | *wq | *wk;
-    let noNoEa:u64 =  (*wn << 17) & !FILE_A ;
-    let noEaEa:u64 =  (*wn << 10) & !FILE_AB;
-    let soEaEa:u64 =  (*wn >>  6) & !FILE_AB;
-    let soSoEa:u64 =  (*wn >> 15) & !FILE_A ;
-    let noNoWe:u64 =  (*wn << 15) & !FILE_H ;
-    let noWeWe:u64 =  (*wn <<  6) & !FILE_GH;
-    let soWeWe:u64 =  (*wn >> 10) & !FILE_GH;
-    let soSoWe:u64 =  (*wn >> 17) & !FILE_H ;
-    (noNoEa | noEaEa | soEaEa | soSoEa | noNoWe | noWeWe | soWeWe | soSoWe) & !white
+
+    let nonoea:u64 =  (*wn << 17) & !FILE_A ;
+    let noeaea:u64 =  (*wn << 10) & !FILE_AB;
+    let soeaea:u64 =  (*wn >>  6) & !FILE_AB;
+    let sosoea:u64 =  (*wn >> 15) & !FILE_A ;
+    let nonowe:u64 =  (*wn << 15) & !FILE_H ;
+    let nowewe:u64 =  (*wn <<  6) & !FILE_GH;
+    let sowewe:u64 =  (*wn >> 10) & !FILE_GH;
+    let sosowe:u64 =  (*wn >> 17) & !FILE_H ;
+    (nonoea | noeaea | soeaea | sosoea | nonowe | nowewe | sowewe | sosowe) & !white
 }
 fn possibility_bn(wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u64, wk:&mut u64, bp:&mut u64, bn:&mut u64, bb:&mut u64, br:&mut u64, bq:&mut u64, bk:&mut u64) -> u64 {
     let black = *bp | *bn | *bb | *br | *bq | *bk;
     //let white = *wp | *wn | *wb | *wr | *wq | *wk;
-    let noNoEa:u64 =  (*bn << 17) & !FILE_A ;
-    let noEaEa:u64 =  (*bn << 10) & !FILE_AB;
-    let soEaEa:u64 =  (*bn >>  6) & !FILE_AB;
-    let soSoEa:u64 =  (*bn >> 15) & !FILE_A ;
-    let noNoWe:u64 =  (*bn << 15) & !FILE_H ;
-    let noWeWe:u64 =  (*bn <<  6) & !FILE_GH;
-    let soWeWe:u64 =  (*bn >> 10) & !FILE_GH;
-    let soSoWe:u64 =  (*bn >> 17) & !FILE_H ;
-    (noNoEa | noEaEa | soEaEa | soSoEa | noNoWe | noWeWe | soWeWe | soSoWe) & !black
+    let nonoea:u64 =  (*bn << 17) & !FILE_A ;
+    let noeaea:u64 =  (*bn << 10) & !FILE_AB;
+    let soeaea:u64 =  (*bn >>  6) & !FILE_AB;
+    let sosoea:u64 =  (*bn >> 15) & !FILE_A ;
+    let nonowe:u64 =  (*bn << 15) & !FILE_H ;
+    let nowewe:u64 =  (*bn <<  6) & !FILE_GH;
+    let sowewe:u64 =  (*bn >> 10) & !FILE_GH;
+    let sosowe:u64 =  (*bn >> 17) & !FILE_H ;
+    (nonoea | noeaea | soeaea | sosoea | nonowe | nowewe | sowewe | sosowe) & !black
 }
 fn convert_move_to_bitboard(moves : &str) -> (u64,u64) {
     let piece = &moves[0..2];
@@ -137,7 +138,6 @@ fn compute_move_w(a:u64, b:u64, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u
     let mut moves= 0;
     if ((*wp) & a) != 0 {
         let mut p = (*wp) & a;
-        
         moves = possibility_wp(&mut p, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
         //println!("M : {:b}", moves);
     }
@@ -164,24 +164,14 @@ fn compute_move_w(a:u64, b:u64, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u
         
     }
     if moves & b != 0 {
-        (*wp) = (*wp) & (!a);
-        (*wp) = (*wp) | b;
+        (*wp) &= !a;
+        (*wp) |= b;
         if black & b != 0 {
-            if *bp & b != 0 {
-                *bp &= !b;
-            }
-            else if *bn & b != 0 {
-                *bn &= !b;
-            }
-            else if *bb & b != 0 {
-                *bb &= !b;
-            }
-            else if *br & b != 0 {
-                *br &= !b;
-            }
-            else if *bq & b != 0 {
-                *bq &= !b;
-            }
+            if *bp & b != 0 { *bp &= !b; }
+            else if *bn & b != 0 { *bn &= !b; }
+            else if *bb & b != 0 { *bb &= !b; }
+            else if *br & b != 0 { *br &= !b; }
+            else if *bq & b != 0 { *bq &= !b; }
         }
         true
     }
@@ -239,9 +229,6 @@ fn compute_move_b(a:u64, b:u64, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u
     else if *wk & a != 0 {
         false
     }
-    else if *wn & a != 0 {
-        false
-    }
     else {
         false
     }
@@ -278,26 +265,26 @@ fn main() {
     //let play_move = "e2e3";
     //let (a,b) = convert_move_to_bitboard(play_move);
     //println!("WP : {:b}", a&wp);
-    let moves = ["e2e3", "d7d6", "e3e4", "d6d5", "e4d5"];
+    //let moves = ["e2e3", "d7d6", "e3e4", "d6d5", "e4d5"];
+    let moves = ["b1c3"];
     draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
-    let now = Instant::now();
+    //let now = Instant::now();
     for m in moves {
         //let mut buffer = String::new();
         //io::stdin().read_line(&mut buffer).unwrap();
-        let (a,b) = convert_move_to_bitboard(&m);
-        let response;
-        if white_to_play {
-            response = compute_move_w(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+        let (a,b) = convert_move_to_bitboard(m);
+        let response = if white_to_play {
+            compute_move_w(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk)
         }
         else {
-            response = compute_move_b(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
-        }
+            compute_move_b(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk)
+        };
         white_to_play ^= response;
         
-        //println!("{:b} \n{:b}",a ,b);
-        //println!("{}", m);
-        //println!("{}", response);
-        //draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+        println!("{a:b} \n{b:b}");
+        println!("{m}");
+        println!("{response}");
+        draw_board(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
     }
-    println!("{} nano seconde", now.elapsed().as_nanos());
+    //println!("{} nano seconde", now.elapsed().as_nanos());
 }
