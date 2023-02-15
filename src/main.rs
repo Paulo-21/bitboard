3435a8c4def6d1d06115774526bd8e6eaa54b28b
Paulo-21 <paul.cibier@gmail.com>
2023-02-16 00:42:13 +0100

    init a legal move generation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use minstant::Instant;
 
 #[cfg(not(target_os = "linux"))]
 use std::time::Instant;
-use bitintr::{Tzcnt, Lzcnt};
+use bitintr::{Tzcnt, Lzcnt, Andn};
 enum Piece {
     PAWN,
     KNIGHT,
@@ -13,6 +13,15 @@ enum Piece {
     ROOK,
     QUEEN,
     KING
+}
+
+fn convert_square_to_move(a_move : u64) -> String{
+    let b = (a_move / 8) as u8;
+    let a:u8 = (a_move % 8) as u8;
+    let f = ('a' as u8 + a ) as char;
+    let mut a = String::from(f);
+    a.push((48 + b+1 ) as char );
+    a
 }
 static FILE_A:u64 = 72340172838076673;
 static FILE_B:u64 = 144680345676153340;
@@ -367,7 +376,7 @@ fn possibility_w( wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64, wq:&mut u6
     let mut attack = 0;
     attack |= possibility_wp(wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
     attack |= possibility_wn(wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
-    let devant = (*wb).clz();
+    let devant = (*wb).lzcnt();
     let arriere = (*wb).tzcnt();
     //let devant = (*wb).leading_zeros();
     //let arriere = (*wb).trailing_zeros();
@@ -424,33 +433,55 @@ fn is_attacked(target_is_w : bool, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mu
         possibility_b(wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk) & *wk != 0
     }
 }
-fn get_legal_move(side_w : bool, wp1:&mut u64, wn1:&mut u64, wb1:&mut u64, wr1:&mut u64, wq1:&mut u64, wk1:&mut u64, bp1:&mut u64, bn1:&mut u64, bb1:&mut u64, br1:&mut u64, bq1:&mut u64, bk1:&mut u64) -> Vec<(u64,u64)> {
+fn get_legal_move(side_w : bool, wp1:&mut u64, wn1:&mut u64, wb1:&mut u64, wr1:&mut u64, wq1:&mut u64, wk1:&mut u64, bp1:&mut u64, bn1:&mut u64, bb1:&mut u64, br1:&mut u64, bq1:&mut u64, bk1:&mut u64) -> Vec<u64> {
     let (mut wp, mut wn, mut wb, mut wr, mut wq, mut wk, mut bp, mut bn, mut bb, mut br, mut bq, mut bk) = copy_bitboard(wp1, wn1, wb1, wr1, wq1, wk1, bp1, bn1, bb1, br1, bq1, bk1);
     let black = *bp1 | *bn1 | *bb1 | *br1 | *bq1 | *bk1;
     let white = *wp1 | *wn1 | *wb1 | *wr1 | *wq1 | *wk1;
     let occupied = black | white;
+    let mut legal_moves = Vec::<u64>::new();
     if side_w {
         //Pions Possibility
         let mut devant = wp.leading_zeros() as u64;
-        let mut derriere = wp.leading_zeros() as u64;
+        let mut derriere = 0;
         let possi_wp = possibility_w(&mut devant, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
-
+        
         //Knight
-        devant = wn.leading_zeros() as u64;
-        derriere = wn.leading_zeros() as u64;
-        let possi_wn = possibility_wn(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
-        let possi_wn2 = if devant != derriere {
-            possibility_wn(&mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk)
-        } else { 0 };
-
-        //Bishop
-        devant = wb.leading_zeros() as u64;
-        derriere = wb.leading_zeros() as u64;
-        let possi_wb = diag_antid_moves(devant, occupied);
-        let possi_wb2 = if devant != derriere {
-            diag_antid_moves(derriere, occupied)
+        //let (mut wp, mut wn, mut wb, mut wr, mut wq, mut wk, mut bp, mut bn, mut bb, mut br, mut bq, mut bk) = copy_bitboard(wp1, wn1, wb1, wr1, wq1, wk1, bp1, bn1, bb1, br1, bq1, bk1);
+        let mut wn_test = *wn1;
+        while wn_test != 0 {
+            let piece = wn_test.tzcnt();
+            let mut wn_extract = (1 << piece) as u64;
+            wn_test = wn_test & wn_test - 1;
+            let mut wn_possi = possibility_wn(&mut wp, &mut wn_extract, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk) & !white;
+            while wn_possi != 0 {
+                let (mut wp, mut wn, mut wb, mut wr, mut wq, mut wk, mut bp, mut bn, mut bb, mut br, mut bq, mut bk) = copy_bitboard(wp1, wn1, wb1, wr1, wq1, wk1, bp1, bn1, bb1, br1, bq1, bk1);
+                let b = wn_possi.tzcnt();
+                compute_move_w(piece, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+                let is_check = is_attacked(true, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+                if !is_check {
+                    legal_moves.push((piece<<8) + b);
+                }
+                wn_possi = wn_possi & wn_possi - 1;
+            }
         }
-        else { 0 };
+        
+        //Bishop
+        let mut wb_test = *wb1;
+        while wb_test != 0 {
+            let piece = wb_test.tzcnt();
+            wb_test = wb_test & wb_test - 1;
+            let mut wb_possi = diag_antid_moves(piece, occupied) & !white;
+            while wb_possi != 0 {
+                let (mut wp, mut wn, mut wb, mut wr, mut wq, mut wk, mut bp, mut bn, mut bb, mut br, mut bq, mut bk) = copy_bitboard(wp1, wn1, wb1, wr1, wq1, wk1, bp1, bn1, bb1, br1, bq1, bk1);
+                let b = wb_possi.tzcnt();
+                compute_move_w(piece, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+                let is_check = is_attacked(true, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+                if !is_check {
+                    legal_moves.push((piece<<8) + b);
+                }
+                wb_possi = wb_possi & wb_possi - 1;
+            }
+        }
 
         //Rook
         devant = wr.leading_zeros() as u64;
@@ -472,7 +503,7 @@ fn get_legal_move(side_w : bool, wp1:&mut u64, wn1:&mut u64, wb1:&mut u64, wr1:&
 
     }
     
-    Vec::from([(6,8)])
+    legal_moves
 }
 fn check_mate() -> bool {
     true
@@ -504,6 +535,12 @@ fn undo_move(a :u64, b: u64, wp:&mut u64, wn:&mut u64, wb:&mut u64, wr:&mut u64,
     
     Ok(())
 }
+fn print_custum_move(a_move : u64) {
+    let a = a_move>>8;
+    let b = a_move & 255;
+    //println!("{a} {b}");
+    println!("{}{}", convert_square_to_move(a), convert_square_to_move(b));
+}
 fn main() {
     let now = Instant::now();
     println!("Instant init : {} nano seconde", now.elapsed().as_nanos());
@@ -532,7 +569,7 @@ fn main() {
     let mut bq : u64 = 0;
     let mut bk : u64 = 0;
     array_to_bitboard(chess_board, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
-
+    
     let mut white_to_play = true;
     //let moves = ["e2e3", "e7e6", "f1d3", "d8g5"];
     //let moves = ["b1c3","g8f6", "c3b1"];
@@ -550,18 +587,28 @@ fn main() {
         
         let now = Instant::now();
         let mut k_attacked = false;
+        let mut legal;
         let response = if white_to_play {
             let a = compute_move_w(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
             k_attacked = is_attacked(true, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+            
+            
             a
         }
         else {
             let a = compute_move_b(a, b, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
             k_attacked = is_attacked(false, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+            //legal = get_legal_move(false, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+            
             a
         };
         white_to_play ^= response;
         println!(" {} nano seconde", now.elapsed().as_nanos());
+        legal = get_legal_move(true, &mut wp, &mut wn, &mut wb, &mut wr, &mut wq, &mut wk, &mut bp, &mut bn, &mut bb, &mut br, &mut bq, &mut bk);
+        for x in legal {
+            print_custum_move(x);
+
+        }
         if k_attacked {
             println!("CHECK");
         }
